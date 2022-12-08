f7757473d6cf6a390f35d2ed2a789cefea85e785
BrennonSullivan <brennsullivan2@gmail.com>
2022-12-07 19:10:38 -0600

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Max
+# Write a function that takes in two numbers and outputs the max (the greater of the two numbers).
---
+++ script.js
@@ -0,0 +1,6 @@
+function max(num1, num2) {
+    // If num1 is greater than num2, return num1.
+    // Otherwise, return num2.
+    return (num1 > num2) ? num1 : num2;
+  }
+  
